16a853f6a220d3c3ad81636854c8a66c9628efd6
eleniv3d <43600924+eleniv3d@users.noreply.github.com>
2020-09-29 08:08:53 +0200

    template testing done

--- example.py
+++ example.py
@@ -12,3 +12,24 @@ def example_method(point_x, point_y):
 
     # After you verified this method works, please delete it ;)
     print('Yay! It works!')
+
+
+def new_method():
+    """test
+
+    """
+
+    for i in range(3):
+        print(i)
+
+
+class myClass():
+
+    def __init__(self, x):
+        self.variable = x
+
+    def my_print(self):
+        print(self.variable)
+
+    def MYTEST(self):
+        print("you didn't correct me")
